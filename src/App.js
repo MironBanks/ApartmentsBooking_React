c5c6ef70ffa4c3ff1ae6b02df995d0b1ef8a4d1c
Miron Banks <madapegarage@gmail.com>
2020-04-07 15:53:51 +0200

    RentalCard Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Shared/Header'
+import RentalHome from './pages/RentalHome'
 
 
 const App = () => {
     return (
         <div>
             <Header />
+            <RentalHome />
         </div>
     )
 }
